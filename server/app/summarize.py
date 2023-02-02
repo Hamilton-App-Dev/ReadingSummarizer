a91212a282325edd7ac27ecea79a5ca5de414fcf
ronnaksaxena <rsaxena@hamilton.edu>
2023-02-02 13:50:58 -0500

    added api endpoint for summarizer

--- server/app/summarize.py
+++
@@ -1,2 +0,0 @@
-def summarize():
-  return "Summarize Function"
--- server/run.py
+++ server/run.py
@@ -1,6 +1,7 @@
-from flask import Flask
-from app.summarize import summarize
-from app.parse import parse
+# Need to downgrade to flask-2.1.3
+from aioflask import Flask, request, Response
+import summarize
+import parse
 
 app = Flask(__name__)
 
@@ -10,9 +11,19 @@ def hello():
   parse()
   return "Hello World"
 
-@app.route("/test", methods = ['GET'])
-def summarize():
-  return "Success!"
+
+'''
+API endpint for local development
+http://127.0.0.1:5000/summarize
+
+Needs params to know what text to summarize
+"readingText" : <text from your reading>
+'''
+@app.route("/summarize", methods = ['GET'])
+def summarizeRoute():
+  readingText = request.args.get('readingText')
+  summary = summarize.summarizeReading(readingText)
+  return summary
 
 if __name__ == "__main__":
   app.run()
