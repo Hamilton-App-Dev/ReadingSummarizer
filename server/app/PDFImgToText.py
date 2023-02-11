32c97d7b684e5a529d522cd0ec3a7272a7f9e15c
derkyeng <dyang@hamilton.edu>
2023-02-11 12:55:03 -0600

    improved preprocessing

--- server/app/PDFImgToText.py
+++ server/app/PDFImgToText.py
@@ -1,29 +1,53 @@
 import cv2
 import pytesseract
 import numpy as np
+from PIL import Image, ImageFilter
+import tempfile
 
-def grayscale(image: np.ndarray):
-    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+IMAGE_SIZE = 1800
+BINARY_THREHOLD = 180
 
-def noise_removal(image: np.ndarray):
+def set_image_dpi(file_path):
+    im = Image.open(file_path)
+    length_x, width_y = im.size
+    factor = max(1, int(IMAGE_SIZE / length_x))
+    size = factor * length_x, factor * width_y
+    # size = (1800, 1800)
+    im_resized = im.resize(size, Image.ANTIALIAS)
+    temp_file = tempfile.NamedTemporaryFile(delete=False, suffix='.jpg')
+    temp_filename = temp_file.name
+    im_resized.save(temp_filename, dpi=(300, 300))
+    return temp_filename
+
+def image_smoothening(img):
+    ret1, th1 = cv2.threshold(img, BINARY_THREHOLD, 255, cv2.THRESH_BINARY)
+    ret2, th2 = cv2.threshold(th1, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    blur = cv2.GaussianBlur(th2, (1, 1), 0)
+    ret3, th3 = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    return th3
+
+def remove_noise_and_smooth(file_name):
+    img = cv2.imread(file_name, 0)
+    filtered = cv2.adaptiveThreshold(img.astype(np.uint8), 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 41,
+                                     3)
     kernel = np.ones((1, 1), np.uint8)
-    image = cv2.dilate(image, kernel, iterations=1)
-    kernel = np.ones((1, 1), np.uint8)
-    image = cv2.erode(image, kernel, iterations=1)
-    image = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel)
-    image = cv2.medianBlur(image, 3)
-    return (image)
+    opening = cv2.morphologyEx(filtered, cv2.MORPH_OPEN, kernel)
+    closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
+    img = image_smoothening(img)
+    or_image = cv2.bitwise_or(img, closing)
+    return or_image
 
-def preprocess_image(image: np.ndarray):
-    grey_image = grayscale(image)
-    thresh, bw_image = cv2.threshold(grey_image, 210, 230, cv2.THRESH_BINARY)
-    image = noise_removal(bw_image)
-    return image
+def process_image_for_ocr(file_path):
+    temp_filename = set_image_dpi(file_path)
+    im_new = remove_noise_and_smooth(temp_filename)
+    return im_new
 
+
+# Input: Give a string list of file urls.
 def read_images(img_files: list):
     text = ""
     for img_file in img_files:
-        image = cv2.imread(img_file)
-        processed_image = preprocess_image(image)
+        processed_image = process_image_for_ocr(img_file)
+        print(processed_image)
         text += pytesseract.image_to_string(processed_image)
     return text
