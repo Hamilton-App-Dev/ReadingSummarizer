aca1a07ba7c0ef55e59c7e8f261d0c6b3273f37c
ronnaksaxena <rsaxena@hamilton.edu>
2023-02-11 11:50:58 -0500

    made script to test converting pdf to png, then converting png to text

---
+++ server/testPdfReader.py
@@ -0,0 +1,15 @@
+import app.PDFImgToText as imgToText
+import app.PDFtoImg as pdfToImg
+import os, os.path
+
+pdfName = 'archReading.pdf' # Change to the name of the pdf you want to test
+dirName = pdfName[:-4]
+pdfToImg.saveImage(pdfName)
+numOfPages = len([name for name in os.listdir(dirName)])
+
+# directory paths to each image
+paths = ['./{}/page_{}.png'.format(dirName, i) for i in range(numOfPages)]
+print(imgToText.read_images(paths))
+
+
+
