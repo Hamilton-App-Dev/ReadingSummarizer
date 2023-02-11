56c149cdfabe84cbc8ff4fcb79ff110dba0bd55d
Daren Hua <lellyo@darens-mbp.hamilton.edu>
2023-02-11 14:03:52 -0500

    finish auth, broken app atm

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import * as dotenv from "dotenv";
+// dotenv.config();
+import { Auth0Provider } from "@auth0/auth0-react";
+
 import App from "./App";
 import "./index.css";
 
@@ -8,7 +12,13 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-    <React.StrictMode>
+    <Auth0Provider
+        domain="dev-ifkribvcg5hqjyhd.us.auth0.com"
+        clientId="BEsy4is3QvOvNsa0CVnzULglN4vfo612"
+        authorizationParams={{
+            redirect_uri: window.location.origin,
+        }}
+    >
         <App />
-    </React.StrictMode>
+    </Auth0Provider>
 );
