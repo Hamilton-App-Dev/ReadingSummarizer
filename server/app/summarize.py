27f44489e158234250ef4ed74be2bd1886a611bb
Riki <ninjab02@hotmail.com>
2023-02-11 12:49:41 -0500

    Added bullet point estimate function & updated askGPT method to use the estimate

--- server/app/summarize.py
+++ server/app/summarize.py
@@ -3,21 +3,39 @@ import os
 
 class Summarizer:
     
-    # Returns a string with your summary attached
     @classmethod
     def askGPT(self,text):
-        global response
+        '''
+        Desc: Returns a string with your summary attached
+        Params: 
+            text:str
+        '''
         openai.api_key = os.getenv("OPENAI_API_KEY")
+        bullet_count = self.get_bullet_estimate(text) 
         response = openai.Completion.create(
             # text-ada-001 -> cheaper but faster
             # text-davinci-003 -> Better quality more expensive
             engine = "text-davinci-003",
-            prompt = 'Summarize this text in 3 bullets {}'.format(text),
+            prompt = f'Summarize this text in {bullet_count} bullets {text}',
             temperature = 0.6,
             max_tokens = 150,
             )
         # Returns a string
         return response.choices[0].text
+
+    @classmethod
+    def get_bullet_estimate(self, text):
+        '''
+        Desc: Estimate the # of bullets needed to summarize the entire text
+        Params: 
+            text:str
+        Returns:
+            bullet_count:int
+        '''
+        word_count = len(text.split(' '))
+        bullet_count = word_count // 150
+        print(bullet_count)
+        return bullet_count
     
     def main1(self):
         pass
@@ -27,3 +45,11 @@ def summarizeReading(text):
     summaryBot = Summarizer()
     return summaryBot.askGPT(text)
 
+
+
+# txt='''
+# Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+# It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+# Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 
+#     '''
+# summarizeReading(txt)
