adb44630a5dcbc4a314aa8e47acedd872f937f8e
Daren Hua <lellyo@Darens-MacBook-Pro.local>
2023-02-13 23:04:21 -0500

    env variables and fixed auth persisting

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,24 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import * as dotenv from "dotenv";
-// dotenv.config();
 import { Auth0Provider } from "@auth0/auth0-react";
 
 import App from "./App";
 import "./index.css";
 
 const root = ReactDOM.createRoot(
-	document.getElementById("root") as HTMLElement
+    document.getElementById("root") as HTMLElement
 );
 
 root.render(
-	<Auth0Provider
-		domain="dev-ifkribvcg5hqjyhd.us.auth0.com"
-		clientId="BEsy4is3QvOvNsa0CVnzULglN4vfo612"
-		authorizationParams={{
-			redirect_uri: window.location.origin,
-		}}
-	>
-		<App />
-	</Auth0Provider>
+    <Auth0Provider
+        domain={import.meta.env.VITE_AUTH0_DOMAIN}
+        clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+        authorizationParams={{
+            redirect_uri: window.location.origin,
+        }}
+        useRefreshTokens={true}
+        cacheLocation="localstorage"
+    >
+        <App />
+    </Auth0Provider>
 );
