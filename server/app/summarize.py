f8e12a22f00e2dd504eb0a4eb49a78fdcc1ff09f
ronnaksaxena <rsaxena@hamilton.edu>
2023-02-16 13:39:56 -0500

    cleaned up functions, removed prints etc

--- server/app/summarize.py
+++ server/app/summarize.py
@@ -17,28 +17,29 @@ class Summarizer:
             # text-ada-001 -> cheaper but faster
             # text-davinci-003 -> Better quality more expensive
             engine = "text-davinci-003",
-            prompt = f'Summarize this text in {bullet_count} bullets {text}',
+            prompt = f'Summarize this text with {bullet_count} bullet points: {text}',
             temperature = 0.6,
             max_tokens = 150,
             )
         return response.choices[0].text
+    
+    @classmethod
+    def get_bullet_estimate(self, text):
+        '''
+        Desc: Estimate the # of bullets needed to summarize the entire text
+        Params: 
+            text:str
+        Returns:
+            bullet_count:int
+        '''
+        word_count = len(text.split(' '))
+        bullet_count = math.ceil(word_count / 150)
+        return bullet_count
 
     
     def main1(self):
         pass
 
-
-def get_bullet_estimate(text):
-    '''
-    Desc: Estimate the # of bullets needed to summarize the entire text
-    Params: 
-        text:str
-    Returns:
-        bullet_count:int
-    '''
-    word_count = len(text.split(' '))
-    bullet_count = math.ceil(word_count / 150)
-    return bullet_count
 
 
 # Will return a stirng summary of text arguemnt
